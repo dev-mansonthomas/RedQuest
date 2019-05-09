f30a4d0761fb3073073f7500d1b51f79f1f315b3
Nicolas Deroche <nicolas.deroche@free.fr>
2019-05-09 09:58:18 +0200

    Add levels for badges. Level=0 means "not earned"

--- src/app/modules/quest/badges/badges.component.ts
+++ src/app/modules/quest/badges/badges.component.ts
@@ -8,22 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class BadgesComponent implements OnInit {
 
   badges = [
-    { icon: 'fa-thumbs-up', label: '1ère quète', earned: true },
-    { icon: 'fa-calendar', label: 'Calendrier', earned: true },
-    { icon: 'fa-flag', label: 'Badge desc', earned: false },
-    { icon: 'fa-comments', label: 'Badge desc', earned: true },
-    { icon: 'fa-ambulance', label: 'Badge desc', earned: false },
-    { icon: 'fa-comments', label: 'Badge desc', earned: false },
-    { icon: 'fa-bone', label: 'Badge desc', earned: false },
-    { icon: 'fa-coins', label: 'Badge desc', earned: true },
-    { icon: 'fa-euro-sign', label: 'Badge desc', earned: false },
-    { icon: 'fa-first-aid', label: 'Badge desc', earned: true },
-    { icon: 'fa-first-aid', label: 'Badge desc', earned: false },
-    { icon: 'fa-frog', label: 'Badge desc', earned: false },
-    { icon: 'fa-hiking', label: 'Badge desc', earned: false },
-    { icon: 'fa-hands-helping', label: 'Badge desc', earned: false },
-    { icon: 'fa-igloo', label: 'Badge desc', earned: true },
-    { icon: 'fa-tv', label: 'Badge desc', earned: false },
+    { icon: 'fa-thumbs-up', label: '1ère quète', level: 1 },
+    { icon: 'fa-calendar', label: 'Calendrier', level: 2 },
+    { icon: 'fa-flag', label: 'Badge desc', level: 0 },
+    { icon: 'fa-comments', label: 'Badge desc', level: 1 },
+    { icon: 'fa-ambulance', label: 'Badge desc', level: 3 },
+    { icon: 'fa-comments', label: 'Badge desc', level: 0 },
+    { icon: 'fa-bone', label: 'Badge desc', level: 0 },
+    { icon: 'fa-coins', label: 'Badge desc', level: 1 },
+    { icon: 'fa-euro-sign', label: 'Badge desc', level: 3 },
+    { icon: 'fa-first-aid', label: 'Badge desc', level: 1 },
+    { icon: 'fa-first-aid', label: 'Badge desc', level: 2 },
+    { icon: 'fa-frog', label: 'Badge desc', level: 0 },
+    { icon: 'fa-hiking', label: 'Badge desc', level: 0 },
+    { icon: 'fa-hands-helping', label: 'Badge desc', level: 0 },
+    { icon: 'fa-igloo', label: 'Badge desc', level: 1 },
+    { icon: 'fa-tv', label: 'Badge desc', level: 2 },
   ];
   constructor() { }
 
