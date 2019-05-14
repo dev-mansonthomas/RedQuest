7524b9da71484f00ae5c40f223b380f96a55c75b
Nicolas Deroche <nicolas.deroche@free.fr>
2019-05-14 20:06:49 +0200

    Draft for home page

--- src/app/modules/registration/registration-needed/registration-needed.component.ts
+++ src/app/modules/registration/registration-needed/registration-needed.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-registration-needed',
-  templateUrl: './registration-needed.component.html'
+  templateUrl: './registration-needed.component.html',
+  styles: [`
+  dt { font-size:2em; color: #f44336; font-weight: bold; padding:20px 0 10px;}
+  dd { font-size:15px;}`]
 })
 export class RegistrationNeededComponent { }
