02b25115efae8e18d79dc69a7edcd778ad84efda
Nicolas Deroche <nicolas.deroche@free.fr>
2019-05-15 09:15:46 +0200

    New design for unregistered users (Home Page)

--- src/app/modules/registration/registration-needed/registration-needed.component.ts
+++ src/app/modules/registration/registration-needed/registration-needed.component.ts
@@ -4,7 +4,9 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-registration-needed',
   templateUrl: './registration-needed.component.html',
   styles: [`
-  dt { font-size:2em; color: #f44336; font-weight: bold; padding:20px 0 10px;}
-  dd { font-size:15px;}`]
+
+  h4, dt { font-size:2em !important; color: #f44336; font-weight: bold !important; padding:20px 0 10px;}
+  mat-icon { font-size:1em !important; padding-right: 15px; }
+  dd { font-size:15px; text-align:justify}`]
 })
 export class RegistrationNeededComponent { }
