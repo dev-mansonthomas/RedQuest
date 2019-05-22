f50e3d1e8a8495911704c2bc4554ed89237b27b2
matthieu_lemonnier <mlemonnier@oui.sncf>
2019-05-22 09:31:51 +0200

    rqAutonmousDepartAndReturn for ul_id 223

--- src/app/modules/quest/my-slots/my-slots.component.ts
+++ src/app/modules/quest/my-slots/my-slots.component.ts
@@ -22,7 +22,7 @@ export class MySlotsComponent implements OnInit {
 
   slotsReadOnly = true;
 
-  ulWithSlotsEditable = [508, 595, 486];
+  ulWithSlotsEditable = [508, 595, 486, 223];
 
 
   constructor(private cloudFunctions: CloudFunctionService,
